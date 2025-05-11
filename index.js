cf9be13b408a8a9aa13db504e88e806f5453c2eb
Hoàng Lưu <kleerking@gmail.com>
2025-05-11 22:48:28 +0700

    Update fix v1.0.7

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.use(cookieParser());
 
 connectDB();
 
-app.use("/shop24h/auth/users", require("./app/routes/auth.routes"));
-app.use("/shop24h/users", require("./app/routes/user.routes"));
-app.use("/shop24h/products", require("./app/routes/product.routes"));
-app.use("/shop24h/admin", require("./app/routes/adminManagement.routes"));
-app.use("/shop24h/dashboard", require("./app/routes/dashboard.routes"));
-app.use("/shop24h/otp-auth", require("./app/routes/otpAuth.routes"));
-app.use("/shop24h/images", require("./app/routes/image.routes"));
-app.use("/shop24h", require("./app/routes/index.routes"));
+app.use("/api/v1/auth/users", require("./app/routes/auth.routes"));
+app.use("/api/v1/users", require("./app/routes/user.routes"));
+app.use("/api/v1/products", require("./app/routes/product.routes"));
+app.use("/api/v1/admin", require("./app/routes/adminManagement.routes"));
+app.use("/api/v1/dashboard", require("./app/routes/dashboard.routes"));
+app.use("/api/v1/otp-auth", require("./app/routes/otpAuth.routes"));
+app.use("/api/v1/images", require("./app/routes/image.routes"));
+app.use("/api/v1", require("./app/routes/index.routes"));
 
 app.all("*", (req, res, next) => {
   const err = new Error(`Can't find ${req.originalUrl} on the server!`);
